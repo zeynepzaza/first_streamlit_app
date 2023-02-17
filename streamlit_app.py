0f1f0d56d61fe58f0aee0feffa1662a33426c80c
zeynepzaza <36365783+zeynepzaza@users.noreply.github.com>
2023-02-17 11:13:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())    # take the json version of the response and normalize it 
     streamlit.dataframe(fruityvice_normalized)    # output it in the screen as a table
     
- except URLError as e:
+except URLError as e:
   streamlit.error()
 
 streamlit.stop()
